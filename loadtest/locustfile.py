8060f3dcbcbe0509d439253be96290f4e3942428
Juan Brero <juaan.b17@hotmail.com>
2026-05-30 15:06:15 -0300

    primer submit

--- loadtest/locustfile.py
+++ loadtest/locustfile.py
@@ -1,3 +1,17 @@
 # TODO: Locust load test
 # from locust import HttpUser, task
 # Hit /health and /api/nodes endpoints
+
+from locust import HttpUser, task, between
+
+class APIUser(HttpUser):
+    wait_time = between(1, 5)
+
+    @task
+    def health(self):
+        self.client.get("/health")
+
+    @task
+    def nodes(self):
+        self.client.get("/api/nodes")
+
